fbdf5309452d059a03fa321cfec83301ba6cf418
Klaus Hott Vidal <klahott@gmail.com>
2012-07-15 23:03:36 -0400

    adding the new generator

--- lib/state_machine/audit_trail/railtie.rb
+++ lib/state_machine/audit_trail/railtie.rb
@@ -1,5 +1,6 @@
 class StateMachine::AuditTrail::Railtie < ::Rails::Railtie
   generators do
     require 'state_machine/audit_trail_generator'
+    require 'state_machine/audit_trail_polymorphic_generator'
   end
 end
