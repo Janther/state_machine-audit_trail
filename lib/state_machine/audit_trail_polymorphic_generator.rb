c61e7f4d11a76935c419a7fdc9f3ed1435a9c120
Klaus Hott Vidal <klahott@gmail.com>
2012-07-15 22:56:54 -0400

    Generator

--- lib/state_machine/audit_trail_polymorphic_generator.rb
+++ lib/state_machine/audit_trail_polymorphic_generator.rb
@@ -1,12 +1,15 @@
 require 'rails/generators'
 
-class StateMachine::AuditTrailGenerator < ::Rails::Generators::Base
+class StateMachine::AuditTrailPolymorphicGenerator < ::Rails::Generators::Base
   
   source_root File.join(File.dirname(__FILE__), 'templates')
-  
+
+  argument :table_name, :default => 'state_machine_transitions'
+
   def create_model
+    @model_name
     record do |m|
-      m.file "models/state_machine_transitions.rb", "app/models/state_machine_transitions.rb"
+      m.file "models/state_machine_transition.rb", "app/models/state_machine_transition.rb"
       m.migration_template "migrate/create_state_machine_transitions.rb", "db/migrate"
     end
   end
